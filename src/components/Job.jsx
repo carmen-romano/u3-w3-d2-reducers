30030f3a72fdd4ad49e2709bdcdf567b6f625736
Carmen Romano <my-9@hotmail.it>
2024-05-07 19:29:18 +0200

    Aggiunto storage redux-persist

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -15,26 +15,31 @@ const Job = ({ data }) => {
       className="mx-0 mt-3 p-3"
       style={{ border: "1px solid #00000033", borderRadius: 4 }}
     >
-      {isLoading && <Spinner animation="grow" />}
       {hasError && (
         <Alert key={"danger"} variant={"danger"}>
           Errore!
         </Alert>
       )}
-      <Col xs={1} className="d-flex align-items-center">
-        <FaStar
-          className={` ${isFavorite ? "stars-selected" : "stars"}`}
-          onClick={() => dispatch(addFavouriteCompany(data.company_name))}
-        />
-      </Col>
-      <Col xs={3} className="d-flex align-items-center">
-        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-      </Col>
-      <Col xs={8}>
-        <a href={data.url} target="_blank" rel="noreferrer">
-          {data.title}
-        </a>
-      </Col>
+      {isLoading ? (
+        <Spinner animation="grow" />
+      ) : (
+        <>
+          <Col xs={1} className="d-flex align-items-center">
+            <FaStar
+              className={` ${isFavorite ? "stars-selected" : "stars"}`}
+              onClick={() => dispatch(addFavouriteCompany(data.company_name))}
+            />
+          </Col>
+          <Col xs={3} className="d-flex align-items-center">
+            <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+          </Col>
+          <Col xs={8}>
+            <a href={data.url} target="_blank" rel="noreferrer">
+              {data.title}
+            </a>
+          </Col>
+        </>
+      )}
     </Row>
   );
 };
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -4,13 +4,31 @@ import favouriteCompanyReduce from "../reducers/favouriteCompanyReduce";
 import mainSearchReduce from "../reducers/mainSearchReduce";
 import getJobsReduce from "../reducers/getJobsReduce";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+//import { encryptTrasform } from "redux-persist-transform-encrypt";
+
 const rootReduce = combineReducers({
   favouriteCompany: favouriteCompanyReduce,
   fetchJobsResult: mainSearchReduce,
   getJobs: getJobsReduce,
 });
+const persistConfig = {
+  key: "root",
+  storage,
+  //trasforms: [
+  // encryptTrasform({
+  // secretKet: process.env.REACT_APP_PERSIST_KEY
+  // }),
+  // ],
+};
 
-const store = configureStore({
-  reducer: rootReduce,
+const persistedReducer = persistReducer(persistConfig, rootReduce);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
-export default store;
+
+export const persistor = persistStore(store);
